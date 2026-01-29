49d4dc1f88333ab8b118fccef6b51a5b34e2d310
ninem4re <77124386+ninem4re@users.noreply.github.com>
2026-01-29 13:23:02 +0100

    Add files via upload

---
+++ src/client/java/lab/ninem4re/crashee/CrasheeClient.java
@@ -0,0 +1,10 @@
+package lab.ninem4re.crashee;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CrasheeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
